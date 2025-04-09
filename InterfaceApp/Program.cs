3484185ade0e1052ba0382c14997c636b0c60318
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-09 13:29:44 +0200

    Add project files.

---
+++ InterfaceApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace InterfaceApp
+{
+    public interface Employee
+    {
+        // interface can contain only abstract methods
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
